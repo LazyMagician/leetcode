0f945be5445f72cb478f4c09f8c582ed88a9721b
Ananthu Achary <67104309+LazyMagician@users.noreply.github.com>
2026-06-06 09:09:17 +0530

    Time: 2 ms (69.27%) | Memory: 46.1 MB (95.76%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.java
@@ -0,0 +1,65 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        boolean [][] row = new boolean [9][9];
+        boolean [][] col = new boolean [9][9];
+        boolean [][] boxes = new boolean [9][9];
+
+        int rowLen = board.length;
+        int colLen = board[0].length;
+        
+        char ele =0;
+
+        // row check
+        for(int i=0;i<rowLen;i++){
+            for(int j=0;j<colLen;j++){
+                ele = board[i][j];
+                if(ele != '.'){
+                    Integer num = ele - '0';
+                    if(row[i][num-1]){
+                        // System.out.println("false row");
+                        return false;
+                    }else{
+                        row[i][num-1]=true;
+                    }
+                }
+            }
+        }  
+
+        //col check
+         for(int j=0;j<colLen;j++){
+            for(int i=0;i<rowLen;i++){
+                ele = board[i][j];
+                if(ele != '.'){
+                    Integer num = ele - '0';
+                    if(col[j][num-1]){
+                        // System.out.println("false col");
+                        return false;
+                    }else{
+                        col[j][num-1]=true;
+                    }
+                }
+            }
+         }
+
+
+         // box check
+        int boxNum = -1;
+           for(int i=0;i<rowLen;i++){
+            for(int j=0;j<colLen;j++){
+                boxNum = (i/3)*3+(j/3);
+                ele = board[i][j];
+                if(ele != '.'){
+                    Integer num = ele - '0';
+                    if(boxes[boxNum][num-1]){
+                        // System.out.println("false box "+i+" "+j+" "+boxNum+" "+(num-1)+" "+boxes[boxNum][num-1]);
+                        return false;
+                    }else{
+                        boxes[boxNum][num-1]=true;
+                    }
+                }
+            }
+        }  
+
+        return true;
+    }
+}
