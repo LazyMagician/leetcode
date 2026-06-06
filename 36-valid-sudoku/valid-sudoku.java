1469dfb1114e4c6a56b1edfa38e57378c037e7cb
Ananthu Achary <67104309+LazyMagician@users.noreply.github.com>
2026-06-06 11:40:41 +0530

    Time: 2 ms (69.27%) | Memory: 45.9 MB (97.70%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -14,7 +14,7 @@ class Solution {
             for(int j=0;j<colLen;j++){
                 ele = board[i][j];
                 if(ele != '.'){
-                    Integer num = ele - '0';
+                    int num = ele - '0';
 
                     // row check
                     if(row[i][num-1]){
@@ -43,42 +43,6 @@ class Solution {
                 }
             }
         }  
-
-        //col check
-        //  for(int j=0;j<colLen;j++){
-        //     for(int i=0;i<rowLen;i++){
-        //         ele = board[i][j];
-        //         if(ele != '.'){
-        //             Integer num = ele - '0';
-        //             if(col[j][num-1]){
-        //                 // System.out.println("false col");
-        //                 return false;
-        //             }else{
-        //                 col[j][num-1]=true;
-        //             }
-        //         }
-        //     }
-        //  }
-
-
-         // box check
-        // int boxNum = -1;
-        //    for(int i=0;i<rowLen;i++){
-        //     for(int j=0;j<colLen;j++){
-        //         boxNum = (i/3)*3+(j/3);
-        //         ele = board[i][j];
-        //         if(ele != '.'){
-        //             Integer num = ele - '0';
-        //             if(boxes[boxNum][num-1]){
-        //                 // System.out.println("false box "+i+" "+j+" "+boxNum+" "+(num-1)+" "+boxes[boxNum][num-1]);
-        //                 return false;
-        //             }else{
-        //                 boxes[boxNum][num-1]=true;
-        //             }
-        //         }
-        //     }
-        // }  
-
         return true;
     }
 }
