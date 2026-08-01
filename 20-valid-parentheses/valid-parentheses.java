3fdf91bb0b319d5e327a315ae24ced816db2492c
Ananthu Achary <67104309+LazyMagician@users.noreply.github.com>
2026-08-01 20:54:30 +0530

    Time: 5 ms (13.35%) | Memory: 43.5 MB (15.55%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,44 @@
+class Solution {
+    public boolean isValid(String s) {
+        if(s.isEmpty())
+        return true;
+        Deque<Character> stack = new ArrayDeque<>();
+
+        for(Character bracket:s.toCharArray()){
+            if(bracket == '(' || bracket == '[' || bracket == '{'){
+                stack.push(bracket);
+            }
+            if(!stack.isEmpty()){
+                if(bracket == ')'){
+                    if(stack.peek() == '(')
+                        stack.pop();
+                    else{
+                        return false;
+                    }
+                }
+                 if(bracket == ']'){
+                    if(stack.peek() == '[')
+                        stack.pop();
+                    else{
+                    return false;
+                    }
+                }else if(bracket == '}'){
+                    if(stack.peek() == '{')
+                        stack.pop();
+                    else{
+                        return false;
+                    }
+                }
+            }else{
+                return false;
+            }
+        } 
+
+        if(stack.isEmpty()){
+            return true;
+        }
+        return false;
+    }
+
+ 
+}
