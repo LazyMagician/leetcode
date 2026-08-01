5192a78c30f5b54431ec16a707909713c9ab23d0
Ananthu Achary <67104309+LazyMagician@users.noreply.github.com>
2026-08-01 21:33:34 +0530

    Time: 33 ms (67.66%) | Memory: 99.9 MB (51.06%) - LeetSync

--- 155-min-stack/min-stack.java
+++ 155-min-stack/min-stack.java
@@ -1,41 +1,29 @@
 class MinStack {
 
-    List<Integer> stack = new ArrayList<>();
-    List<Integer> minStack = new ArrayList<>();
-    int stackLen = 0;
-    int minStackLen = 0;
+    Stack<Integer> stack = new Stack<Integer>();
+    Stack<Integer> minStack = new Stack<Integer>();
     public MinStack() {
+        
     }
     
     public void push(int value) {
-        stack.add(value);
-        stackLen++;
-        if(minStackLen == 0 ){
-            minStack.add(value);
-            minStackLen++;
-        }
-        else if( minStack.get(minStackLen-1) >= value){
-            minStack.add(value);
-            minStackLen++;
-        }
+        stack.push(value);
+        if(minStack.isEmpty() || minStack.peek() >= value)
+            minStack.push(value);
     }
     
     public void pop() {
-        int topEle = stack.get(stackLen-1);
-        stack.remove(stackLen-1);
-        stackLen--;
-        if(minStackLen != 0 && minStack.get(minStackLen-1) == topEle){
-            minStack.remove(minStackLen-1);
-            minStackLen--;
-        }    
+        int topEle = stack.pop();
+        if(minStack.peek() == topEle)
+            minStack.pop();
     }
     
     public int top() {
-       return  stack.get(stackLen-1);
+        return stack.peek();
     }
     
     public int getMin() {
-            return  minStack.get(minStackLen-1);
+        return minStack.peek();
     }
 }
 
