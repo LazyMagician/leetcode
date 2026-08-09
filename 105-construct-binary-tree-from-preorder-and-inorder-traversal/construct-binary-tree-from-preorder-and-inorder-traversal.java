48aaaa8b7601b8483751b7d79971826da78059d9
Ananthu Achary <67104309+LazyMagician@users.noreply.github.com>
2026-08-09 08:02:09 +0530

    Time: 5 ms (9.50%) | Memory: 45.3 MB (93.39%) - LeetSync

---
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        return buildBinaryTree(preorder,inorder,0,0,inorder.length-1);
+    }
+
+
+    public TreeNode buildBinaryTree(int[] preorder, int[] inorder, int preIdx, int inStart, int inEnd){
+        // System.out.println("preIdx - preorder.length - inStart - inEnd");
+
+        // System.out.println(preIdx+" "+preorder.length +" "+inStart +" "+ inEnd);
+        if(preIdx >= preorder.length || inStart > inEnd ) return null;
+
+        TreeNode root = new TreeNode(preorder[preIdx]);
+
+        Integer rootIdx = findIdx(inorder, root.val, inStart, inEnd);
+
+        Integer numLeftTreeEle = rootIdx - inStart;
+        root.left = buildBinaryTree(preorder,inorder, preIdx+1, inStart, rootIdx-1);
+        root.right = buildBinaryTree(preorder, inorder, preIdx+numLeftTreeEle+1,rootIdx+1,inEnd);
+
+        return root;
+    }
+
+    public Integer findIdx(int[] arr, int value, int start, int end){
+        // System.out.println(start+" "+end);
+        for(int i=start;i<=end;i++){
+            if(arr[i] == value) return i;
+        }
+        return -1;
+    }
+}
