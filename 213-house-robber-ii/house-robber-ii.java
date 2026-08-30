2a792c6411af8ae40c8499e243890a6d659c75d6
Ananthu Achary <67104309+LazyMagician@users.noreply.github.com>
2026-08-30 17:23:47 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (41.59%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int rob(int[] nums) {
+        int houselen = nums.length;
+        if( houselen == 1 ) return nums[0];
+        int firstHouse =nums[0], secondHouse = Integer.max(nums[0],nums[1]), currHouse = 0;
+
+        if( houselen == 2 ) return secondHouse;
+        currHouse = Integer.max(firstHouse,secondHouse);
+        for(int i=2;i<houselen-1;i++){
+            currHouse = Integer.max(firstHouse+nums[i],secondHouse);
+            firstHouse = secondHouse;
+            secondHouse = currHouse;
+        } 
+        int withFirstHouse = currHouse;
+        firstHouse = 0;
+        secondHouse = nums[1];
+        for(int i=2;i<houselen;i++){
+            currHouse = Integer.max(firstHouse+nums[i],secondHouse);
+            firstHouse = secondHouse;
+            secondHouse = currHouse;
+        } 
+
+        return Integer.max(currHouse,withFirstHouse);
+
+    }
+}
