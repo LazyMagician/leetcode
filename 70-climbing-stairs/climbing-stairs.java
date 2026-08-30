e0b123b09df6d2a10ebd155a89f08de7bb285b0c
Ananthu Achary <67104309+LazyMagician@users.noreply.github.com>
2026-08-30 14:45:15 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (17.37%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int climbStairs(int n) {
+        Integer[] dp = new Integer[n+1];
+        
+        dp[0] = 1;
+        dp[1]  = 1;
+
+        for(int i =2;i<=n;i++){
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+
+        // for(int i=0;i<=n;i++){
+        //     System.out.println("step "+i+" : "+dp[i]);
+        // }
+
+        return dp[n];
+    }
+}
